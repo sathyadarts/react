17e0d4c43630b6a6c37c1db4f05a2402061373cb
sathyadarts <sathyaseelandarts007@gmail.com>
2023-05-24 16:50:29 +0530

    first commit

---
+++ sathya/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Sample from './components/Sample';
+
+function App() {
+  return (
+    <div className="App">
+      <Sample></Sample>
+    </div>
+  );
+}
+
+export default App;
---
+++ sathya/src/components/Sample.js
@@ -0,0 +1,5 @@
+import React from 'react'
+function Sample(){
+    return <h1>hello world</h1>
+}
+export default Sample
